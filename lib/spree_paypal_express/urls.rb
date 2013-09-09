f2cc56fca36cc4e0a699f229ea0f583cbea17dc1
Martin Tepper <mgpalmer@monogreen.de>
2013-09-09 10:21:30 +0200

    Add possibility to override linkout hosts

--- lib/spree_paypal_express/urls.rb
+++ lib/spree_paypal_express/urls.rb
@@ -7,11 +7,18 @@ module SpreePaypalExpress
         
     def paypal_urls(order, payment_method)
       host = request.host
-      host = "192.168.2.112"
+      host = development_paypal_linkout_host(host) if Rails.env.development?
       port = request.port
       return_url = Spree::Core::Engine.routes.url_helpers.paypal_confirm_order_checkout_url(order, :payment_method_id => payment_method.id, :host => host, :port => port)
       cancel_url = Spree::Core::Engine.routes.url_helpers.paypal_cancel_order_checkout_url(order, :payment_method_id => payment_method.id, :host => host, :port => port)
       [return_url, cancel_url]
+    end
+    
+    # To let users override the host for the urls handed to paypal, in case
+    # you need to link to something other than "localhost" for testing.
+    # Only used in development mode.
+    def development_paypal_linkout_host(host)
+      host
     end
     
   end
